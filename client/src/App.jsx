1f91514ee724dc1b0dccf389edd16e9b52d86145
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-16 04:15:17 +0530

    Creating Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
-  return <div>{!isOwnerPath && <Navbar />}</div>;
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
