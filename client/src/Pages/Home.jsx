26590dac290a9e0d267365aeb0444472cd98c52d
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-20 20:03:31 +0530

    Creating the Newsletter Section

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import FeaturedDestination from "../components/FeaturedDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import Testimonial from "../components/Testimonial";
+import Newletter from "../components/Newletter";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedDestination />
       <ExclusiveOffers />
       <Testimonial />
+      <Newletter />
     </>
   );
 };
