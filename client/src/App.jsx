d1c02245ea099a56c91e17bf9ede72992baf2f12
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-25 00:23:50 +0530

    Creating Owner Layout Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,10 @@ import AllRooms from "./Pages/AllRooms";
 import RoomDetails from "./Pages/RoomDetails";
 import MyBookings from "./Pages/MyBookings";
 import HotelReg from "./components/HotelReg";
+import Layout from "./Pages/hotelOwner/Layout";
+import Dashboard from "./Pages/hotelOwner/Dashboard";
+import AddRoom from "./Pages/hotelOwner/AddRoom";
+import ListRoom from "./Pages/hotelOwner/ListRoom";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -20,6 +24,11 @@ const App = () => {
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
---
+++ client/src/Pages/hotelOwner/AddRoom.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddRoom = () => {
+  return <div></div>;
+};
+
+export default AddRoom;
